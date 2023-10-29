96bc853383d77fdb275d4a4a13b0b6f861d79c71
Ozoani Chidubem <flyinglego153971@gmail.com>
2023-10-29 19:40:22 +0100

    connected server endpoint to static site

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import socketIO from 'socket.io-client'
 const PORT = 3100
-export const socket = socketIO.connect("https://p2p-video-call-16ju.onrender.com" || `http://localhost:${PORT}`)
+export const socket = socketIO.connect(`http://localhost:${PORT}` || "https://p2p-video-call-16ju.onrender.com" )
