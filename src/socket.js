3d869b645e267a51a653d4f1d93117a418084d27
Ozoani Chidubem <flyinglego153971@gmail.com>
2023-10-29 19:56:42 +0100

    fixed server endpoint error

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import socketIO from 'socket.io-client'
 const PORT = 3100
-export const socket = socketIO.connect(`http://localhost:${PORT}` || "https://p2p-video-call-16ju.onrender.com" )
+export const socket = socketIO.connect(window.location.hostname === "localhost" ? `http://localhost:${PORT}` : "https://p2p-video-call-16ju.onrender.com")
